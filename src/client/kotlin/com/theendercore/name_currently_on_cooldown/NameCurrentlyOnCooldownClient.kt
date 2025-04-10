dca53e5d480f42990d6b40b5158a18f7a484b39c
TheEnderCore <theendercore@gmail.com>
2025-04-10 18:52:06 +0300

    - made bar thinner - moved old bar texture to built-in resource pack

--- src/client/kotlin/com/theendercore/name_currently_on_cooldown/NameCurrentlyOnCooldownClient.kt
+++ src/client/kotlin/com/theendercore/name_currently_on_cooldown/NameCurrentlyOnCooldownClient.kt
@@ -2,9 +2,14 @@ package com.theendercore.name_currently_on_cooldown
 
 import me.fzzyhmstrs.fzzy_config.api.RegisterType
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType
+import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.ModContainer
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.function.Consumer
 
 @Suppress("unused")
 object NameCurrentlyOnCooldownClient {
@@ -16,7 +21,19 @@ object NameCurrentlyOnCooldownClient {
     fun initialize() {
         cataloger.info("Cooling you down!")
         if (CONFIGURATION.enabledModification) HudRenderCallback.EVENT.register(::renderCooldownIndicator)
+
+        registerBuiltInPack(MODIFICATION_IDENTIFIER, identifier("old_bar"))
     }
+
+    // TODO: Replace with VoidLib
+    private fun registerBuiltInPack(
+        modId: String, id: Identifier, packType: ResourcePackActivationType = ResourcePackActivationType.NORMAL
+    ) = useMod(modId) {
+        assert(ResourceManagerHelper.registerBuiltinResourcePack(id, it, packType))
+        { "Failed to register built-in pack \"$id\" !" }
+    }
+    private fun useMod(id: String, consumer: Consumer<ModContainer>) =
+        FabricLoader.getInstance().getModContainer(id).ifPresent(consumer)
 
     fun identifier(path: String): Identifier = Identifier.of(MODIFICATION_IDENTIFIER, path)
 }
