31ff137de555ac55f103b29bb7b967a1fe938106
TheEnderCore <theendercore@gmail.com>
2025-07-02 15:04:17 +0300

    updated to new hud render api

--- src/client/kotlin/com/theendercore/name_currently_on_cooldown/NCOCClient.kt
+++ src/client/kotlin/com/theendercore/name_currently_on_cooldown/NCOCClient.kt
@@ -3,7 +3,8 @@ package com.theendercore.name_currently_on_cooldown
 import com.theendercore.name_currently_on_cooldown.config.NameCurrentlyOnCooldownConfig
 import me.fzzyhmstrs.fzzy_config.api.ConfigApi
 import me.fzzyhmstrs.fzzy_config.api.RegisterType
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType
 import net.fabricmc.loader.api.FabricLoader
@@ -20,7 +21,9 @@ object NCOCClient {
     val cataloger: Logger = LoggerFactory.getLogger(NCOCClient::class.simpleName)
     fun initialize() {
         cataloger.info("Cooling you down!")
-        if (CONFIG.enabledMod) HudRenderCallback.EVENT.register(::renderCooldownIndicator)
+        if (CONFIG.enabledMod) HudElementRegistry.attachElementAfter(
+            VanillaHudElements.CROSSHAIR, id("cooldown_indicator"), ::renderCooldownIndicator
+        )
 
         registerBuiltInPack(MODID, id("old_bar"))
     }
